90a2bc582ddb16fb4d84084c737628caf70f2b10
Lukáš Jiřička <jiricka.f.lukas@gmail.com>
2019-03-14 17:12:38 +0100

    Ošetření chyb

--- grep.py
+++ grep.py
@@ -1,11 +1,27 @@
 #!/usr/bin/env python3
+"""Usage: grep.py PATTERN FILE
+
+Print lines from FILE matching regular expression PATTERN.
+"""
+
 
 import sys
 import re
 
-print(sys.argv)
-pattern, path = sys.argv[1:]
-with open(path) as file:
-    for line in file:
-        if re.search(pattern, line):
-            print(line, end="")
+try:
+    pattern, path = sys.argv[1:]
+except ValueError as err:
+    print(__doc__.strip(), file=sys.stderr)
+    print(err, file=sys.stderr)
+    sys.exit(1)
+# standard error "neteče rourou"
+
+try:
+    with open(path) as file:
+        for line in file:
+            if re.search(pattern, line):
+                print(line, end="")
+except FileNotFoundError as err:
+    print(__doc__.strip(), file=sys.stderr)
+    print(err, file=sys.stderr)
+    sys.exit(1)
